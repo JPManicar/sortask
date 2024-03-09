97ae3e97ae89a5c2a9e615a44ac0ac6352a6cd62
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-03-09 09:16:26 +0800

    [BE] Add models

--- backend/core/sortask/models.py
+++ backend/core/sortask/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_by = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name='projects')
+
+
+class Board(models.Model):
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, related_name='boards')
+    name = models.CharField(max_length=255)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    board = models.ForeignKey(
+        Board, on_delete=models.CASCADE, related_name='tasks')
+    due_date = models.DateTimeField()
+    assignee = models.ForeignKey(get_user_model(),
+                                 related_name='tasks', on_delete=models.SET_NULL, null=True, blank=True)
+    created_by = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name='created_tasks')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class CheckList(models.Model):
+    content = models.CharField(max_length=255)
+    is_completed = models.BooleanField()
+    task = models.ForeignKey(
+        Task, on_delete=models.CASCADE, related_name='checklists')
+
+
+class Comments(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    task = models.ForeignKey(
+        Task, on_delete=models.CASCADE, related_name='comments')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Members(models.Model):
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, related_name='members')
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name='projects')
