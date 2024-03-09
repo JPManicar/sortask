c93e280dceeef6d9da4add9c9cbcfa2edc7d78a2
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-03-10 00:59:22 +0800

    [BE] Add auth permission for api endpoints

--- backend/sortask/views.py
+++ backend/sortask/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from .models import Project, Board, Task, CheckList, Comment, Member
 from .serializers import (ProjectSerializer, BoardSerializer, TaskSerializer,
                           CheckListSerializer, CommentSerializer, MemberSerializer)
@@ -7,28 +8,34 @@ from .serializers import (ProjectSerializer, BoardSerializer, TaskSerializer,
 class ProjectViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class BoardViewSet(ModelViewSet):
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class ChecklistViewSet(ModelViewSet):
     queryset = CheckList.objects.all()
     serializer_class = CheckListSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class CommentViewSet(ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class MemberViewSet(ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
+    permission_classes = [IsAuthenticated]
