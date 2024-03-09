7f548a030c1b5f9fb540a1fa0dfe531085bccefa
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-03-09 09:59:10 +0800

    [BE] Add intial views

--- backend/core/sortask/views.py
+++ backend/core/sortask/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Project, Board, Task, Checklist,  Comment, Member
+from .serializers import (ProjectSerializer, BoardSerializer, TaskSerializer,
+                          ChecklistSerializer, CommentSerializer, MemberSerializer)
 
-# Create your views here.
+
+class ProjectViewSet(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class BoardViewSet(ModelViewSet):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
+
+
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class ChecklistViewSet(ModelViewSet):
+    queryset = Checklist.objects.all()
+    serializer_class = ChecklistSerializer
+
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class MemberViewSet(ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
