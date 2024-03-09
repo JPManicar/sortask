10eef769a2ae7fda2c50843b5049d9b24ebb0607
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-03-10 00:55:42 +0800

    [BE] Add auth serializers

---
+++ backend/core/serializers.py
@@ -0,0 +1,13 @@
+from djoser.serializers import UserSerializer as BaseUserSerializer, UserCreateSerializer as BaseUserCreateSerializer
+
+
+# Creation of user
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields = ['id', 'username', 'password', 'email', 'first_name', 'email']
+
+
+# Retrieving of user
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
