4fa24f3df5008d41181f25e295b55553d14154c3
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-03-13 19:05:06 +0800

    Add authorization in task viewset

--- backend/sortask/views/task.py
+++ backend/sortask/views/task.py
@@ -4,6 +4,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from ..models import Task
 from ..serializers import TaskSerializer, TaskListSerializer
+from ..permissions import check_permission
 
 
 class TaskViewSet(ModelViewSet):
@@ -14,9 +15,22 @@ class TaskViewSet(ModelViewSet):
     def get_serializer_class(self):
         return TaskListSerializer if self.action == 'list' else TaskSerializer
 
+    def check_project_id(self, request):
+        project_id = request.query_params.get('project_id')
+        if not project_id:
+            return Response({'error': 'parameter project_id is required'}, status=status.HTTP_400_BAD_REQUEST)
+        return project_id
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
+
+            response = check_permission(
+                self.request.user, serializer.validated_data['project'])
+
+            if response:
+                return response
+
             serializer.save()
             return Response({
                 'status': 'Successful'
@@ -28,6 +42,36 @@ class TaskViewSet(ModelViewSet):
                 new_error[field_name] = field_errors[0]
             return Response(new_error, status=status.HTTP_400_BAD_REQUEST)
 
+    def list(self, request):
+        project_id = self.check_project_id(request)
+
+        if isinstance(project_id, Response):
+            return project_id
+
+        response = check_permission(self.request.user, project_id)
+
+        if response:
+            return response
+
+        serializer = self.get_serializer(self.get_queryset(), many=True)
+
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        project_id = self.check_project_id(request)
+
+        if isinstance(project_id, Response):
+            return project_id
+
+        response = check_permission(self.request.user, project_id)
+
+        if response:
+            return response
+
+        serializer = self.get_serializer(self.get_queryset(), many=True)
+
+        return Response(serializer.data)
+
     def perform_create(self, serializer):
         instance = serializer.save(created_by=self.request.user)
 
@@ -35,10 +79,13 @@ class TaskViewSet(ModelViewSet):
 
     def get_queryset(self):
         queryset = Task.objects.none()
-        project_id = self.request.query_params.get('project_id')
-        if project_id:
-            queryset = queryset.union(Task.objects.filter(
-                project=project_id, project__members__user=self.request.user))
+
+        project_id = self.check_project_id(self.request)
+        if isinstance(project_id, Response):
+            return project_id
+
+        queryset = queryset.union(Task.objects.filter(
+            project=project_id, project__members__user=self.request.user))
 
         assignee_ids = self.request.query_params.getlist('assignee_ids')
         if assignee_ids:
@@ -48,3 +95,16 @@ class TaskViewSet(ModelViewSet):
                 Task.objects.filter(assignee__id__in=assignee_ids))
 
         return queryset
+
+    def destroy(self, request):
+        project_id = self.check_project_id(request)
+
+        if isinstance(project_id, Response):
+            return project_id
+
+        response = check_permission(self.request.user, project_id)
+
+        if response:
+            return response
+
+        return super().destroy(request)
