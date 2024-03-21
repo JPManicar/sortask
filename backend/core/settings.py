76edfce711a2b8e58c451f774a771476ef7270bb
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-03-21 21:44:47 +0800

    Add django-filter

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'djoser',
     'sortask',
-    'debug_toolbar'
+    'debug_toolbar',
+    'django_filters'
 
 ]
 
@@ -139,6 +140,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 SPECTACULAR_SETTINGS = {
